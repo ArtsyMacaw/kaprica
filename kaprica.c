5849a9a0971eb11d6f2a022a864b65c370798dfe
Haden Collins <collinshaden@gmail.com>
2024-04-20 00:35:00 -0500

    Add error message when no data is passed for copying

--- kaprica.c
+++ kaprica.c
@@ -631,6 +631,7 @@ int main(int argc, char *argv[])
         {
             if (!get_stdin(num_of_args, args, src))
             {
+                fprintf(stderr, "Nothing to copy\n");
                 goto cleanup;
             }
         }
